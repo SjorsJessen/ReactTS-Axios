80488dbaffaae687c0f7d04367b2bf5d8d44e4a2
Sjors Jessen <sjors.jessen@gmail.com>
2021-04-07 18:23:45 +0200

    Cancelling requests with Axios

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-import axios from "axios";
+import axios, { CancelTokenSource } from "axios";
 
 interface IPost {
     userId: number;
@@ -13,38 +13,57 @@ interface IPost {
 interface IState {
     posts: IPost[];
     error: string;
+    cancelTokenSource?: CancelTokenSource;
+    loading: boolean;
 }
 
 class App extends React.Component<{}, IState> {
     public constructor(props: {}) {
         super(props);
         this.state = {
+            loading: true,
             posts: [],
             error: ""
         }
     };
 
     public componentDidMount() {
+        const cancelToken = axios.CancelToken;
+        const cancelTokenSource = cancelToken.source();
+        this.setState({cancelTokenSource});
+        
         axios.get<IPost[]>("https://jsonplaceholder.typicode.com/posts", {
+            cancelToken: cancelTokenSource.token,
             headers: {
                 "Content-Type" : "application/json"
             },
-            timeout: 1
+            timeout: 5000
         })
             .then(response => {
-                this.setState({ posts: response.data });
-                console.log("Posts: " + this.state.posts);
+                this.setState({ posts: response.data, loading: false });
             })
             .catch(ex => {
-            const error = ex.code === "ECONNABORTED" ? "A timeout has occured" : 
+            const error = axios.isCancel(ex) ? "Request cancelled" : 
+                          ex.code === "ECONNABORTED" ? "A timeout has occured" : 
                           ex.response.status === 404 ? "Resource not found" : "An unexpected error has occurred";
-            this.setState({ error });
+            this.setState({ error, loading: false });
         });
+
+        cancelTokenSource.cancel("User cancelled operation");
     }
 
+    private handleCancelClick = () => {
+        if (this.state.cancelTokenSource) {
+            this.state.cancelTokenSource.cancel("User cancelled operation");
+        }
+    };
+    
     public render() {
         return (
             <div className="App">
+                {this.state.loading && (
+                    <button onClick={this.handleCancelClick}>Cancel</button>
+                )}
                 <ul className="posts">
                     {this.state.posts.map(post => (
                         <li key={post.id}>
