f6f827dbf17bdf9325891ea0a0e660e0b9760d8f
Sjors Jessen <sjors.jessen@gmail.com>
2021-04-08 22:18:38 +0200

    Formatting

--- src/App.tsx
+++ src/App.tsx
@@ -22,12 +22,7 @@ const App: React.FC = () => {
     
     // @ts-ignore
     const [loading, setLoading]: [boolean, (loading: boolean) => void] = React.useState(false);
-    const [editPost, setEditPost]: [IPost, (post: IPost) => void] =
-        React.useState({
-            body: "",
-            title: "",
-            userId: 1
-        });
+    const [editPost, setEditPost]: [IPost, (post: IPost) => void] = React.useState({body: "", title: "", userId: 1});
 
     React.useEffect(() => {
         axios
@@ -73,33 +68,16 @@ const App: React.FC = () => {
         if (editPost.id) {
             axios
                 .put<IPost>(`https://jsonplaceholder.typicode.com/posts/${editPost.id}`, editPost,
-                    {
-                        headers: {
-                            "Content-Type": "application/json"
-                        }
-                    }
+                    {headers: {"Content-Type": "application/json"}}
                 )
-                .then(() => {
-                    setEditPost({
-                        body: "",
-                        title: "",
-                        userId: 1
-                    });
+                .then(() => {setEditPost({body: "", title: "", userId: 1});
                     setPosts(posts.filter(post => post.id !== editPost.id).concat(editPost));
                 });
         } else {
             axios
                 .post<IPost>("https://jsonplaceholder.typicode.com/posts",
-                    {
-                        body: editPost.body,
-                        title: editPost.title,
-                        userId: editPost.userId
-                    },
-                    {
-                        headers: {
-                            "Content-Type": "application/json"
-                        }
-                    }
+                    {body: editPost.body, title: editPost.title, userId: editPost.userId},
+                    {headers: {"Content-Type": "application/json"}}
                 )
                 .then(response => {setPosts(posts.concat(response.data));});
         }
@@ -112,38 +90,24 @@ const App: React.FC = () => {
     const handleDeleteClick = (post: IPost) => {
         axios
             .delete(`https://jsonplaceholder.typicode.com/posts/${post.id}`)
-            .then(() => {
-                setPosts(posts.filter(p => p.id !== post.id));
-            });
+            .then(() => {setPosts(posts.filter(p => p.id !== post.id));});
     };
 
     return (
         <div className="App">
             <div className="post-edit">
-                <input
-                    type="text"
-                    placeholder="Enter title"
-                    value={editPost.title}
-                    onChange={handleTitleChange}
-                />
-                <textarea
-                    placeholder="Enter body"
-                    value={editPost.body}
-                    onChange={handleBodyChange}
-                />
+                <input type="text" placeholder="Enter title" value={editPost.title} onChange={handleTitleChange}/>
+                <textarea placeholder="Enter body" value={editPost.body} onChange={handleBodyChange}/>
                 <button onClick={handleSaveClick}>Save</button>
             </div>
-            {loading && <button
-                onClick={handleCancelClick}>Cancel</button>}
+            {loading && <button onClick={handleCancelClick}>Cancel</button>}
             <ul className="posts">
                 {posts.map(post => (
                     <li key={post.id}>
                         <h3>{post.title}</h3>
                         <p>{post.body}</p>
-                        <button onClick={() =>
-                            handleUpdateClick(post)}>Update</button>
-                        <button onClick={() =>
-                            handleDeleteClick(post)}>Delete</button>
+                        <button onClick={() => handleUpdateClick(post)}>Update</button>
+                        <button onClick={() => handleDeleteClick(post)}>Delete</button>
                     </li>
                 ))}
             </ul>
